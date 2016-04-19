068f8351bcf4403ca106961b550cf70679f89373
Colin MacPhail <colinmacphail@gmail.com>
2016-04-18 20:26:56 -0700

    hopefuly working now

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,25 @@
 class Product < ActiveRecord::Base
+
+  def friendly_created_at
+    return created_at.strftime("%b %e, %l:%M %p")
+  end
+
+  def sales_message
+    if price < 2
+      return "Discount item!"
+    else
+      return "On sale!"
+    end
+  end
+
+  def tax
+    tax = price * 0.09
+    return tax
+  end
+
+  def total
+    "#{tax} + #{price}"
+  end
+
+
 end
